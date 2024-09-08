9f898132ed5000f922a2dc206020c41c5177996b
Gabriel Antonio <gabriel.antonio.dlm@gmail.com>
2024-09-08 16:23:42 -0300

    fix: Implementado limite de 6 pokemons por time

--- backend/src/app.js
+++ backend/src/app.js
@@ -58,6 +58,12 @@ app.get('/api/:keyword', async (req, res) => {
 // Método para adicionar um Pokémon ao time
 app.post('/api/:keyword', async (req, res) => {
     const { keyword } = req.params;
+
+    // Verifica se o time já possui 6 Pokémons
+    if (team.length == 6) {
+        return res.status(400).json({ message: 'Seu time já possui o limite de 6 Pokémons.', team });
+    }
+
     try {
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${keyword}`);
         const pokemonData = response.data;
