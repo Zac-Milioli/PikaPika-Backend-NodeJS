64522814344bbcbf2a0c0458b7bc301d0d872e4a
Eduardo Kipper <eduardo.k.rubio@gmail.com>
2024-09-08 20:29:21 -0300

    Front-end Finalizado com a função de Buscar e Estética

--- frontend/index.js
+++ frontend/index.js
@@ -11,6 +11,12 @@ async function getTeam() {
         pokeTeam.innerHTML = ""; //Limpa a div do time para evitar de ficar repetindo
         for (let i = 0; i < result.length; i++) {
     
+            let anchor = document.createElement('a');
+            anchor.href = "";
+            anchor.onclick = function() {
+                deletePokemon(img.id); // Passa o id da imagem (nome do pokemon) como parametro para a função deletePokemon
+            }; // Deleta o pokemon do time ao clicar
+
             // Cria um novo elemento de imagem com a sprite do pokemon, texto alternativo, id e titulo contendo o nome do pokemon
             let img = document.createElement('img');
             img.src = result[i].img;
@@ -18,11 +24,9 @@ async function getTeam() {
             img.id = result[i].nome;
             img.title = result[i].nome;
             img.height = 60;// Define a altura da imagem para 60 px
-            img.onclick = function() {
-                deletePokemon(img.id); // Passa o id da imagem (nome do pokemon) como parametro para a função deletePokemon
-            }; // Deleta o pokemon do time ao clicar
-
-            pokeTeam.appendChild(img); // Adiciona a imagem dentro da div pokeTeam
+            
+            anchor.appendChild(img)
+            pokeTeam.appendChild(anchor); // Adiciona a imagem dentro da div pokeTeam
         };
         return result;
     } catch (error) {
@@ -30,16 +34,42 @@ async function getTeam() {
     }
 }
 
+async function getPokemon(pokemon) {
+    pokemon = pokemon.toLowerCase()
+    try {
+        const response = await fetch(`${url}/api/${pokemon}`, {
+            method: 'GET',
+        });
+        const result = await response.json();
+        console.log(result.pokemonTipo)
+        document.getElementById('pokemonImage').src = result.pokemonImg
+        document.getElementById('pokedexNumber').innerHTML = result.pokemonPokedex
+        document.getElementById('pokemonName').title = result.pokemonNome
+        document.getElementById('pokemonName').innerHTML = result.pokemonNome
+        document.getElementById('firstType').innerHTML = result.pokemonTipo[0]
+        document.getElementById('firstType').className = `ms-1 ${result.pokemonTipo[0]}`
+        document.getElementById('secondType').innerHTML = ""
+        document.getElementById('secondType').className = ""
+        if (result.pokemonTipo[1]){
+            document.getElementById('secondType').innerHTML = result.pokemonTipo[1]
+            document.getElementById('secondType').className = result.pokemonTipo[1]
+        }
+        document.getElementById('pokemonInput').value = ""; // limpa o campo de pesquisa
+    } catch (error) {
+        console.error('Error:', error.message); // Não está retornando o erro não sei o pq
+    }
+}
+
 async function postPokemon() {
     let team = await getTeam(); // Armazena a lista de times fornecida pela função getTeam
-    let pokemon = document.getElementById('pkInput').value // Recebe o valor do pokemon preenchido pelo usuário (será alterado pelo nome do pokemon buscado)
+    let pokemon = document.getElementById('pokemonName').title // Recebe o valor do pokemon preenchido pelo usuário (será alterado pelo nome do pokemon buscado)
     if (pokemon != "" && team.length < 6) {
         pokemon = pokemon.toLowerCase(); // Transforma o nome do pokemon em minúsculo
         try{
             const response = await fetch(`${url}/api/${pokemon}`, {
                 method: 'POST',
             });
-            document.getElementById('pkInput').value = ""; // limpa o campo de pesquisa
+            
             getTeam();
         } catch (error) {
             console.error(error); // Não está retornando o erro não sei o pq 
