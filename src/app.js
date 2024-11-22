9444e738a15ddc6462df35517f2d54dc04bf854e
Eduardo Kipper <eduardo.k.rubio@gmail.com>
2024-11-22 20:46:39 -0300

    teste CI - 3

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ const axios = require('axios');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// const Users = require('./models/users');
-// const Teams = require('./models/teams');
+const Users = require('./models/users');
+const Teams = require('./models/teams');
 
 const app = express();
 app.use(cors());
@@ -15,14 +15,14 @@ app.use(express.json());
 const PORT = process.env.PORT;
 
 //connect mongodb
-// mongoose.connect(process.env.MONGODB_URI)
-//   .then(() => console.log('Connected to MongoDB Atlas'))
-//   .catch(err => console.error('Could not connect to MongoDB Atlas', err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('Could not connect to MongoDB Atlas', err));
 
-// //retorna pagina inicial
-// app.get('/', (req, res) => {
-//     res.sendFile('views/index.html', { root: __dirname });
-// });
+//retorna pagina inicial
+app.get('/', (req, res) => {
+    res.sendFile('views/index.html', { root: __dirname });
+});
 
 //integrantes do grupo
 const data = {
@@ -35,131 +35,131 @@ const data = {
     ]
 };
 
-// //metodo chamar integrantes
-// app.get('/integrantes', (req, res) => {
-//     res.json(data);
-// });
+//metodo chamar integrantes
+app.get('/integrantes', (req, res) => {
+    res.json(data);
+});
 
-// //metodo buscar pokemons
-// app.get('/api/:idPokemon', async (req, res) => {
-//     const idPokemon = req.params.idPokemon; 
-//     try {
-//         const pokeApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`);
-//         const pokemonData = pokeApi.data;
+//metodo buscar pokemons
+app.get('/api/:idPokemon', async (req, res) => {
+    const idPokemon = req.params.idPokemon; 
+    try {
+        const pokeApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`);
+        const pokemonData = pokeApi.data;
 
-//         // Extrai as informações necessárias do Pokémon
-//         const pokemonNome = pokemonData.name;
-//         const pokemonPokedex = pokemonData.id;
-//         const pokemonTipo = pokemonData.types.map(typeInfo => typeInfo.type.name);
-//         const pokemonImg = pokemonData.sprites.front_default;
+        // Extrai as informações necessárias do Pokémon
+        const pokemonNome = pokemonData.name;
+        const pokemonPokedex = pokemonData.id;
+        const pokemonTipo = pokemonData.types.map(typeInfo => typeInfo.type.name);
+        const pokemonImg = pokemonData.sprites.front_default;
 
-//         // Retorna as informações do Pokémon em formato JSON
-//         res.json({ message: 'Informações do Pokemon número ' + pokemonPokedex, 
-//             pokemonNome, 
-//             pokemonPokedex, 
-//             pokemonTipo, 
-//             pokemonImg });
-//     } catch (error) {
-//         if (error.response && error.response.status === 404) {
-//             return res.status(404).json({ message: 'Pokémon não encontrado na PokeAPI' });
-//         }
-//         res.status(500).json({ message: 'Erro ao buscar Pokémon', error: error.message });
-//     }
-// });
+        // Retorna as informações do Pokémon em formato JSON
+        res.json({ message: 'Informações do Pokemon número ' + pokemonPokedex, 
+            pokemonNome, 
+            pokemonPokedex, 
+            pokemonTipo, 
+            pokemonImg });
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            return res.status(404).json({ message: 'Pokémon não encontrado na PokeAPI' });
+        }
+        res.status(500).json({ message: 'Erro ao buscar Pokémon', error: error.message });
+    }
+});
 
-// // Endpoint para salvar o time do usuário
-// app.post('/user/:userId', async (req, res) => {
-//     const userId = req.params.userId;
-//     const team = req.body.team;
+// Endpoint para salvar o time do usuário
+app.post('/user/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    const team = req.body.team;
 
-//     try {
-//         // Faz a busca pelo usuário
-//         let time = await Teams.findOne({ userId});
+    try {
+        // Faz a busca pelo usuário
+        let time = await Teams.findOne({ userId});
 
-//         if (time) {  
-//             time.team = team; 
-//             await time.save();
-//             return res.status(200).json({ message: 'Time atualizado com sucesso', time }); 
-//         } else { 
-//             // Cria um novo time
-//             console.log({userId, team}); 
-//             time = await Teams.create({ userId, team }); 
+        if (time) {  
+            time.team = team; 
+            await time.save();
+            return res.status(200).json({ message: 'Time atualizado com sucesso', time }); 
+        } else { 
+            // Cria um novo time
+            console.log({userId, team}); 
+            time = await Teams.create({ userId, team }); 
 
-//             return res.status(201).json({ message: 'Time criado com sucesso', time });
-//         }
-//     } catch (error) {
-//         res.status(500).json({ message: 'Erro ao criar time', error: error.message });
-//     }
-// });
+            return res.status(201).json({ message: 'Time criado com sucesso', time });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao criar time', error: error.message });
+    }
+});
 
-// // Endpoint para retornar o time de um usuário específico
-// app.get('/user/:userId', async (req, res) => {
-//     const userId = req.params.userId;
+// Endpoint para retornar o time de um usuário específico
+app.get('/user/:userId', async (req, res) => {
+    const userId = req.params.userId;
 
-//     try {
-//         // Busca o time do usuário
-//         const userTeam = await Teams.findOne({ userId });
+    try {
+        // Busca o time do usuário
+        const userTeam = await Teams.findOne({ userId });
 
-//         const team = [
-//             { id: 0, nome: "", img: "" },
-//             { id: 1, nome: "", img: "" },
-//             { id: 2, nome: "", img: "" },
-//             { id: 3, nome: "", img: "" },
-//             { id: 4, nome: "", img: "" },
-//             { id: 5, nome: "", img: "" },
-//         ];
+        const team = [
+            { id: 0, nome: "", img: "" },
+            { id: 1, nome: "", img: "" },
+            { id: 2, nome: "", img: "" },
+            { id: 3, nome: "", img: "" },
+            { id: 4, nome: "", img: "" },
+            { id: 5, nome: "", img: "" },
+        ];
 
-//         if (!userTeam) {
-//             await Users.create({userId});
-//             var newTeam = await Teams.create({userId, team});
-//             return res.json(newTeam);
-//         }
+        if (!userTeam) {
+            await Users.create({userId});
+            var newTeam = await Teams.create({userId, team});
+            return res.json(newTeam);
+        }
 
-//         return res.json(userTeam);
-//     } catch (error) {
-//         res.status(500).json({ message: 'Erro ao buscar time', error: error.message });
-//     }
-// });
+        return res.json(userTeam);
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao buscar time', error: error.message });
+    }
+});
 
-// // Endpoint para deletar o time de um usuário
-// app.delete('/user/:userId', async (req, res) => {
-//     const userId = req.params.userId;
+// Endpoint para deletar o time de um usuário
+app.delete('/user/:userId', async (req, res) => {
+    const userId = req.params.userId;
 
-//     try {
-//         // Deleta o time do usuário
-//         const deletedTeam = await Teams.findOneAndDelete({ userId });
+    try {
+        // Deleta o time do usuário
+        const deletedTeam = await Teams.findOneAndDelete({ userId });
 
-//         if (!deletedTeam) {
-//             return res.status(404).json({ message: 'Time não encontrado' });
-//         }
+        if (!deletedTeam) {
+            return res.status(404).json({ message: 'Time não encontrado' });
+        }
 
-//         const deletedUser = await Users.findOneAndDelete({ userId });
+        const deletedUser = await Users.findOneAndDelete({ userId });
 
-//         if (!deletedUser) {
-//             return res.status(404).json({ message: 'Usuário não encontrado' });
-//         }
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
         
-//         // Retorna a estrutura padrão do time
-//         const defaultTeam = [
-//             { id: 0, nome: "", img: "" },
-//             { id: 1, nome: "", img: "" },
-//             { id: 2, nome: "", img: "" },
-//             { id: 3, nome: "", img: "" },
-//             { id: 4, nome: "", img: "" },
-//             { id: 5, nome: "", img: "" },
-//         ];
+        // Retorna a estrutura padrão do time
+        const defaultTeam = [
+            { id: 0, nome: "", img: "" },
+            { id: 1, nome: "", img: "" },
+            { id: 2, nome: "", img: "" },
+            { id: 3, nome: "", img: "" },
+            { id: 4, nome: "", img: "" },
+            { id: 5, nome: "", img: "" },
+        ];
 
-//         res.json({ message: 'Time deletado com sucesso', team: defaultTeam });
-//     } catch (error) {
-//         res.status(500).json({ message: 'Erro ao deletar time', error: error.message });
-//     }
-// });
+        res.json({ message: 'Time deletado com sucesso', team: defaultTeam });
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao deletar time', error: error.message });
+    }
+});
 
-// // Inicia o servidor na porta especificada
-// if (require.main === module) {
-//     app.listen(PORT, '0.0.0.0', () => {
-//         console.log(`Servidor rodando na porta ${PORT}`);
-//     });
-// }
+// Inicia o servidor na porta especificada
+if (require.main === module) {
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+    });
+}
 
-// module.exports = app; // Exporta o aplicativo
+module.exports = app; // Exporta o aplicativo
